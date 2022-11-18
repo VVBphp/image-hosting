6c57e9cc42ea6e85953c67232ccfc140c314250b
VVBphp <VVB@vvb>
2022-11-18 19:38:55 +0300

    dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,23 @@ module.exports = {
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
     ],
-
+    darkMode: 'class',
     theme: {
+        darkSelector: '.dark',
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+    },
+    variants: {
+        backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd', 'hover', 'responsive'],
+        borderColor: ['dark', 'dark-focus', 'dark-focus-within', 'hover', 'responsive'],
+        textColor: ['dark', 'dark-hover', 'dark-active', 'hover', 'responsive'],
+        boxShadow: ['dark','responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
+        extend: {
+            cursor: ['disabled','hover']
+        }
     },
 
     plugins: [require('@tailwindcss/forms')],
